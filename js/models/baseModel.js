9939091a42e934ea4714fa40bc3eedb12298e2c8
Rajat <rajataroara@gmail.com>
2015-09-15 00:40:19 +0530

    Localstorage issue when unit data change

--- js/models/baseModel.js
+++ js/models/baseModel.js
@@ -28,6 +28,10 @@ var BaseModel = (function() {
                 eachItem = this._comparedItems[uniqueIdentifier];
                 if (this._rootdata) {
                     var listingItem = this._rootdata.towers[eachItem.towerIdentifier].listings[eachItem.unitIdentifier] || {};
+                    if(JSON.stringify(listingItem) == '{}') {
+                        utils.removeShortlistedUnit(eachItem.unitUniqueIdentifier);
+                        return {};
+                    }
                     item = this._rootdata.towers[eachItem.towerIdentifier].rotationAngle[eachItem.rotationAngle].listing[eachItem.unitIdentifier];
                     item[item.unitIdentifier] = item;
                     item.bookingAmount = listingItem.bookingAmount;
