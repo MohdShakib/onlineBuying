2664d40eb91309ff7e9e4b37c067041f14e0b801
Mohit-Tyagi <mohit.kumartyagi.mca@gmail.com>
2016-02-18 12:46:28 +0530

    Chnage API & Zip to local for testing purpose.

--- js/configs/config.js
+++ js/configs/config.js
@@ -6,9 +6,9 @@ var config = (function() {
         urlAppName : '/4d-view',
 
         // Independent App
-        apisJson: false,
+        apisJson: true,
         cityJson: true, // to read cities from json
-        localZip: false,
+        localZip: true,
         setJsonDataPriorityForTest: false,
         builderSetUp: false,
         showInterestedIn: true,       // to show hide interested popup
