9374d3479cd5966b69c9bce9a76fdb82bcc8773b
Mohit-Tyagi <mohit.kumartyagi.mca@gmail.com>
2016-03-01 15:19:41 +0530

    Added amenities name support for displaying color icon

--- js/views/masterplanView.js
+++ js/views/masterplanView.js
@@ -937,10 +937,10 @@ var MasterplanView = (function () {
             for (var amenityKey in data.amenities) {
                 if (hasOwnProperty.call(data.amenities, amenityKey)) {
                     var amenity = data.amenities[amenityKey];
-                    var displayIcon = amenity.displayIcon ? 'icon-'+amenity.displayIcon : '';
+                    var iconPathCode = viewUtils.getIconHtml(amenity.amenityName);
                     var point = data.amenities[amenityKey].amenitySvg.split(' ');
                     var position = "top:" + point[1] + "%; left:" + point[0] + "%;";
-                    code += "<div data-top='" + point[1] + "' data-left='" + point[0] + "' id='" + amenityKey + "' class='" + config.amenityIconClass + "' style='" + position + "'><span class='icon  icon-location "+ displayIcon +" transition fs0'></span>";
+                    code += "<div data-top='" + point[1] + "' data-left='" + point[0] + "' id='" + amenityKey + "' class='" + config.amenityIconClass + "' style='" + position + "'>" + iconPathCode ;
                     code += "<div class='name'><img class='amenity-img' src=" + amenity.imageUrl + "><span>" + amenity.amenityName + "</span></div>";
                     code += "</div>";
                 }
